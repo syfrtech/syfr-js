2cddbca167d4f0b7bcb3f2260ae8b7b20550590b
Jonathan Stanley <jonathan@bristleconeweb.com>
2021-12-06 13:44:02 -0500

    BREAKING CHANGE: new release path

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,51 @@
 /**
- *
  * @see https://webpack.js.org/
- *
- * integrity to allow us to host the js
- * @see https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity
- * @see https://github.com/waysact/webpack-subresource-integrity/tree/main/examples/webpack-assets-manifest/
- * @see https://github.com/webdeveric/webpack-assets-manifest#integrity
- *
- * will likely need to update crossorigin
- * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/crossorigin
- *
- * @todo should restore contenthash in filename to ensure never repeat?
  */
 const path = require("path");
 const dotenv = require("dotenv").config({ path: "./.env" });
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const { SubresourceIntegrityPlugin } = require("webpack-subresource-integrity");
 const WebpackAssetsManifest = require("webpack-assets-manifest");
 var PACKAGE = require("./package.json");
-var version = PACKAGE.version;
 
-module.exports = {
-  mode: "production",
-  devtool: false,
-  entry: {
-    syfr: path.resolve(__dirname, "src/index.ts"),
-  },
-  output: {
-    filename: `js/syfr-${version}.min.js`,
-    path: path.resolve(__dirname, "dist"),
-    library: { name: "Syfr", type: "var" },
-    // hashFunction: "sha256",
-    // hashDigestLength: 256,
-    // crossOriginLoading: "anonymous",
-    clean: {
-      keep: /js\//,
+module.exports = (env) => {
+  var version = PACKAGE.version;
+  let outputDir = env.production ? "public" : ".cache";
+  let watch = !env.production;
+  let baseName = "formCipher";
+  return {
+    mode: "production",
+    devtool: "source-map",
+    entry: {
+      [baseName + ".min"]: path.resolve(__dirname, "src/index.ts"),
     },
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
+    output: {
+      filename: `${baseName}.min.js`,
+      path: path.resolve(__dirname, `${outputDir}/${version}`),
+      library: { name: "Syfr", type: "var" },
+      crossOriginLoading: "anonymous",
+      clean: {},
+    },
+    watch,
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      // new HtmlWebpackPlugin({
+      //   filename: "demo.html",
+      //   template: "src/template.html",
+      //   syfrFormId: dotenv.parsed.SYFR_FORM_ID,
+      //   minify: false,
+      // }),
+      new WebpackAssetsManifest({ integrity: true }),
     ],
-  },
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     filename: "demo.html",
-  //     template: "src/template.html",
-  //     syfrFormId: dotenv.parsed.SYFR_FORM_ID,
-  //     minify: false,
-  //     // inject: "body",
-  //   }),
-  //   // using subresourceintegrity causes js to fail to load in browser
-  //   // new SubresourceIntegrityPlugin(),
-  //   new WebpackAssetsManifest({ integrity: true }),
-  // ],
+  };
 };
