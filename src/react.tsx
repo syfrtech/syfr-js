3eb3d2ebe14562af71db528dd35b19f642812ada
Jonathan Stanley <jonathan@bristleconeweb.com>
2022-01-24 20:49:26 -0500

    fix: correct infinte loop

--- src/react.tsx
+++ src/react.tsx
@@ -1,36 +1,37 @@
 import React from "react";
 import { SyfrClass } from "./class";
-import { SyfrFormId } from "./types";
 
 type ReactFormProps = React.DetailedHTMLProps<
   React.FormHTMLAttributes<HTMLFormElement>,
   HTMLFormElement
 >;
 
-export const useSyfrForm = (id: SyfrFormId) => {
-  let [form, setForm] = React.useState<HTMLFormElement | null>();
+export const useSyfrForm = (
+  id: NonNullable<ConstructorParameters<typeof SyfrClass>[1]>,
+  handleForm?: (form: HTMLFormElement) => void
+) => {
   let linkProps = {
     rel: "",
     href: `https://syfr.app/validate/${id}`,
     ["data-syfr-validate"]: true,
   };
 
-  const SyfrForm = ({ action, ...props }: ReactFormProps) => {
-    React.useEffect(() => {
-      if (!!form) new SyfrClass(form, id);
-    }, [form]);
+  const SyfrForm = ({ action, onSubmit, ...props }: ReactFormProps) => {
     // iOS needs an "action" attribute for nice input: https://stackoverflow.com/a/39485162/406725
     const _action = action ?? "#";
-    if (typeof window === "undefined") return <React.Fragment />;
     return (
       <form
-        ref={(element) => {
-          setForm(element);
+        ref={(form) => {
+          if (!!form) {
+            new SyfrClass(form, id);
+            !!handleForm && handleForm(form);
+          }
         }}
         action={_action}
         {...props}
       />
     );
   };
-  return [SyfrForm, form, linkProps] as const;
+
+  return [SyfrForm, linkProps] as const;
 };
